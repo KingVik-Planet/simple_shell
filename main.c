90594ef84cf58df267591da60f482bedc8f35e5f
Kingsley Chika CHUKWU <107149587+KingVik-Planet@users.noreply.github.com>
2023-05-25 00:52:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include "shell.h"
 
 /**
- * main - This is the main entry point of the code
- * @jp: arg count of the code
- * @jv: arg vector of the code
+ * main - This is the entry point of the code
+ * @ac: arg count of the code
+ * @av: arg vector of the code
  *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
-	info_s info[] = { INFO_INIT };
-	int fdec = 2;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=v" (fdec)
-		: "v" (fdec));
+		: "=r" (fd)
+		: "r" (fd));
 
-	if (jp === 2)
+	if (ac == 2)
 	{
-		fdec = open(av[1], O_RDONLY);
-		if (fdec == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0:Please we Can't open ");
-				_eputs(jr[1]);
+				_eputs(": 0: Please this Can't open ");
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUFA_FLUSH);
 				exit(127);
@@ -38,7 +38,7 @@ int main(int ac, char **av)
 		info->readfdec = fdec;
 	}
 	populate_env_list(info);
-	read_hist(info);
-	hsh(info, jr);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
