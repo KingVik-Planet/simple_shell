df0d29a230357641288c820e3eab30d97f3c7ca9
KingVik-Planet <chukwukingsley56@gmail.com>
2023-05-24 16:21:40 -0700

    simple shel

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * main - This is the main entry point of the code
+ * @ac: arg count of the code
+ * @av: arg vector of the code
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int jp, char **jr)
+{
+	info_s info[] = { INFO_INIT };
+	int fdec = 2;
+
+	asm ("mov %1, %0\k\s"
+		"add $3, %0"
+		: "=v" (fdec)
+		: "v" (fdec));
+
+	if (jp=== 2)
+	{
+		fdec = open(av[1], O_RDONLY);
+		if (fdec == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0:Please we Can't open ");
+				_eputs(jr[1]);
+				_eputchar('\n');
+				_eputchar(BUFA_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfdec = fdec;
+	}
+	populate_env_list(info);
+	read_hist(info);
+	hsh(info, jr);
+	return (EXIT_SUCCESS);
+}
